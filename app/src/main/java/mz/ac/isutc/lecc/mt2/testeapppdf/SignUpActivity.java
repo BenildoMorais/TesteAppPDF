acf1c4c99c8ed3870781f3dcc299e81409ca5216
BenildoMorais <benildomorais360@gmail.com>
2023-05-26 08:53:24 +0200

    layout de login, escolha do tipo de usuario

--- app/src/main/java/mz/ac/isutc/lecc/mt2/testeapppdf/SignUpActivity.java
+++ app/src/main/java/mz/ac/isutc/lecc/mt2/testeapppdf/SignUpActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import mz.ac.isutc.lecc.mt2.testeapppdf.databinding.ActivitySignUpBinding;
 
@@ -21,6 +22,26 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        binding.tipoUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (binding.nao.getVisibility() == View.VISIBLE){
+                    binding.nao.setVisibility(View.GONE);
+                    binding.sim.setVisibility(View.VISIBLE);
+
+                    binding.disciplina.setVisibility(View.VISIBLE);
+                    binding.telefone.setVisibility(View.VISIBLE);
+
+                }else {
+                    binding.nao.setVisibility(View.VISIBLE);
+                    binding.sim.setVisibility(View.GONE);
+
+                    binding.disciplina.setVisibility(View.GONE);
+                    binding.telefone.setVisibility(View.GONE);
+                }
             }
         });
     }
